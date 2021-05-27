ae6799ce6ebb947bb14963d4c4e00c03b745a6a9
Jack <gskwon@maxst.com>
2021-05-27 19:07:54 +0900

    Add recruit panel

--- Assets/Chapter5_Resource/Scripts/GameManager.cs
+++ Assets/Chapter5_Resource/Scripts/GameManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,7 +9,14 @@ public class GameManager : MonoBehaviour
 {
     public long money;
     public long moneyIncreaseAmount;
+    public long moneyIncreaseLevel;
+    public long moneyIncreasePrice;
+    public int employeeCount;
     public Text textMoney;
+    public Text textPrice;
+    public Text textRecruit;
+    public Button buttonPrice;
+    public Button buttonRecruit;
     public GameObject prefabMoney;
 
     void Start()
@@ -21,6 +29,15 @@ public class GameManager : MonoBehaviour
     {
         ShowInfo();
         MoneyIncrease();
+        UpdatePanelText();
+        ButtonActiveCheck();
+        UpdateRecruitPanelText();
+        ButtonRecruitActiveCheck();
+    }
+
+    private void ButtonRecruitActiveCheck()
+    {
+        buttonRecruit.interactable = (money >= AutoWork.autoIncreasePrice);
     }
 
     private void MoneyIncrease()
@@ -46,5 +63,39 @@ public class GameManager : MonoBehaviour
         {
             textMoney.text = money.ToString("###,###") + "원";
         }
+    }
+
+    private void UpdatePanelText()
+    {
+        textPrice.text = "Lv." + moneyIncreaseLevel + " 단가 상승\n\n";
+        textPrice.text += "외주 당 단가>\n";
+        textPrice.text += moneyIncreaseAmount.ToString("###,###") + " 원\n";
+        textPrice.text += "업그레이드 가격>\n";
+        textPrice.text += moneyIncreasePrice.ToString("###,###") + " 원";
+    }
+
+    public void UpgradePrice()
+    {
+        if (money >= moneyIncreasePrice)
+        {
+            money -= moneyIncreasePrice;
+            moneyIncreaseLevel += 1;
+            moneyIncreaseAmount += moneyIncreaseLevel * 100;
+            moneyIncreasePrice += moneyIncreaseLevel * 500;
+        }
+    }
+
+    private void ButtonActiveCheck()
+    {
+        buttonPrice.interactable = (money >= moneyIncreasePrice);
+    }
+
+    private void UpdateRecruitPanelText()
+    {
+        textRecruit.text = "Lv." + employeeCount + " 직원 고용\n\n";
+        textRecruit.text += "직원 1초 당 단가 > \n";
+        textRecruit.text += AutoWork.autoMoneyIncreaseAmount.ToString("###,###") + " 원\n";
+        textRecruit.text += "업그레이드 가격 > \n";
+        textRecruit.text += AutoWork.autoIncreasePrice.ToString("###,###") + " 원\n";
     }
 }
